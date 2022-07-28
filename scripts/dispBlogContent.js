4a823e226aaaf023db6c071d5627ea269319e56d
Mike Mo <mo2195054@gmail.com>
2022-07-28 11:05:55 -0400

    updated path structure and improved blog css

--- scripts/dispBlogContent.js
+++ scripts/dispBlogContent.js
@@ -4,6 +4,7 @@ var mainContainer = document.getElementById("blogContent");
 for (i = 0; i < fetchedContent.length; i++) {
     var div = document.createElement("div");
     div.className = "ablogincontent";
+    div.style = "background-color:" + fetchedContent[i].color;
     div.innerHTML = '<div class="blogtitle">'
         + '<a href="' + fetchedContent[i].location + '">'
         + fetchedContent[i].title
